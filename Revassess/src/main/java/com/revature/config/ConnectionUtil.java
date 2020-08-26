ff527de3a6a51601f55fa55fb7e47b71fba05ac3
Laura <lauvillarreal@berkeley.edu>
2020-08-26 14:48:01 -0700

    connected to database

--- Revassess/src/main/java/com/revature/config/ConnectionUtil.java
+++ Revassess/src/main/java/com/revature/config/ConnectionUtil.java
@@ -16,11 +16,11 @@ public class ConnectionUtil {
 	private static ConnectionUtil cu;
 	
 	// add your jdbc url
-	public static final String URL = "";
+	public static final String URL = "jdbc:postgresql://postgresql-class.cks98gmxels6.us-west-1.rds.amazonaws.com:5432/revassess";
 	// add your jdbc username
-	public static final String USERNAME = "";
+	public static final String USERNAME = "associate";
 	// add your jdbc password
-	public static final String PASSWORD = "";
+	public static final String PASSWORD = "password";
 	// name of the created stored procedure in tier 3
 	public static final String TIER_3_PROCEDURE_NAME = "";
 	// name of the created sequence in tier 3
