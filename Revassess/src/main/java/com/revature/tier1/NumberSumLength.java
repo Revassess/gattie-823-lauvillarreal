97838ce1db0b0c2b133b2e5ee6ebc902a6bbb3d8
Laura <lauvillarreal@berkeley.edu>
2020-08-26 12:30:11 -0700

    all but Q2

--- Revassess/src/main/java/com/revature/tier1/NumberSumLength.java
+++ Revassess/src/main/java/com/revature/tier1/NumberSumLength.java
@@ -1,8 +1,36 @@
 package com.revature.tier1;
 
+import java.util.*;
+
 public class NumberSumLength {
 
 	public static boolean checkNumberPowerLength(long num) {
-		return false;
+		System.out.println("num =" + num);
+		long sum = 0;
+		if (num == 1) {
+			System.out.println("yes");
+			return true;
+		}
+		ArrayList<Long> list = new ArrayList<>();
+		long NUM = num;
+		while (NUM > 0) {
+			long val = NUM%10;
+			list.add(val);
+			NUM = NUM/10;
+		}
+		for (Long i : list) {
+			sum += Math.pow(i, list.size());
+			System.out.println(sum);
+		
+		}
+		System.out.println(sum+ " " + num);
+		if (sum == num) {
+			System.out.println("true");
+			return true;
+		} else {
+			System.out.println("false");
+			return false;
+		}
 	}
+
 }
--- Revassess/src/main/java/com/revature/tier1/User.java
+++ Revassess/src/main/java/com/revature/tier1/User.java
@@ -1,4 +1,11 @@
 package com.revature.tier1;
 
 public class User {
+
+	public User() {
+		super();
+	}
+	
+	
+	
 }
